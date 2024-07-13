2720ee31bf5568ab52020bf935bb1601d91bc3af
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-13 12:58:31 +0300

    fixed issue

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,17 +16,47 @@ class LRUCache(BaseCaching):
             return
 
         if len(self.order) == self.MAX_ITEMS and key not in self.order:
-            print("DISCARD: " + str(self.order[0]))
+            old_key = self.order[0]
+            print("DISCARD: " + str(old_key))
+            del self.cache_data[old_key]
             self.order.pop(0)
 
-        try:
+        if key in self.order:
             self.order.remove(key)
-        finally:
-            self.order.append(key)
-            self.cache_data[key] = item
+
+        self.order.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
         """ get item from chace """
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
+
+
+my_cache = LRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
