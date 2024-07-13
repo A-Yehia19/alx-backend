0388b7e33ec47983e73fd8f4658e3c2d2ba7ced6
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-13 11:59:50 +0300

    test case

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,7 +5,7 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
-    """ FIFO Cache class """
+    """ LIFO Cache class """
     def __init__(self):
         """ constructor """
         super().__init__()
