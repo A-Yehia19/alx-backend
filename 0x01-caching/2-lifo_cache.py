43eceb3cde4fed26c7147b150387b1f95aa4b227
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-13 11:57:55 +0300

    fixd issue

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""module doc"""
+from queue import LifoQueue
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ FIFO Cache class """
+    def __init__(self):
+        """ constructor """
+        super().__init__()
+        self.stack = LifoQueue(maxsize=self.MAX_ITEMS)
+
+    def put(self, key, item):
+        """ put item in cache """
+        if key is None or item is None:
+            return
+
+        if self.queue.full():
+            old_key = self.queue.get()
+            del self.cache_data[old_key]
+            print("DISCARD: " + str(old_key))
+        self.queue.put(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get item from chace """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
