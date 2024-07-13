4e0ecb667629c86c494efda63b3bd152320cb8f9
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-07-13 11:49:02 +0300

    fifo

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""module doc"""
+from queue import Queue
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFO Cache class """
+    def __init__(self):
+        """ constructor """
+        super().__init__()
+        self.queue = Queue(maxsize=self.MAX_ITEMS)
+
+    def put(self, key, item):
+        """ put item in cache """
+        if key is None or item is None:
+            return
+
+        if self.queue.full():
+            self.queue.get()
+        self.queue.put(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ get item from chace """
+        if key is None or key not in self.cache_data.keys():
+            return None
+        return self.cache_data[key]
